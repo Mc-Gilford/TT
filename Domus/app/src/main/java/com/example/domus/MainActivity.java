66d26706bc9439034b2c2657a5084f686919486b
McGilfordJose <34671406+McGilfordJose@users.noreply.github.com>
2022-04-17 00:05:22 -0500

    Domus correcto

---
+++ Domus/app/src/main/java/com/example/domus/MainActivity.java
@@ -0,0 +1,38 @@
+
+package com.example.domus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.register);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Do something in response to button click
+                Intent intent = new Intent(MainActivity.this, Register.class);
+                System.out.println("Jola");
+                startActivity(intent);
+            }
+        });
+    }
+    /** Called when the user taps the Send button */
+    /*public void sendMessage(View view) {
+        Intent intent = new Intent(this, Register.class);
+        Button editText = (Button) findViewById(R.id.register);
+        String message = editText.getText().toString();
+        intent.putExtra("HOLA", message);
+        startActivity(intent);
+    }*/
+
+}
